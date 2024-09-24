79e96fdf395a96896ea3e859f930883749e79552
darknessInMe <vladyslav.didukh.22@gmail.com>
2024-09-24 14:54:17 +0300

    add loading state

--- packages/next-app/src/app/page.tsx
+++ packages/next-app/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { HomePageGrid } from "@/features/HomePageGrid";
+import { Suspense } from "react";
 
 export default function Home() {
    return (
       <div>
-         <HomePageGrid />
+         <Suspense fallback={<h1>Loading...</h1>}>
+            <HomePageGrid />
+         </Suspense>
       </div>
    );
 }
