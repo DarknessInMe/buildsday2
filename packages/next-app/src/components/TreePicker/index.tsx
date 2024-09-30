86a16be662ce8125d71aedf6d360792ada200667
darknessInMe <vladyslav.didukh.22@gmail.com>
2024-09-30 16:58:28 +0300

    style tree picker

--- packages/next-app/src/components/TreePicker/index.tsx
+++ packages/next-app/src/components/TreePicker/index.tsx
@@ -2,9 +2,13 @@
 
 import { useMemo } from "react";
 import { useBuilderContext } from "@/context/BuilderContext";
+import clsx from 'clsx';
+
+const isActiveClassName = "bg-white text-black";
 
 export const TreePicker = () => {
-   const { serializedTreeRef, changeCurrentTree } = useBuilderContext();
+   const { serializedTreeRef, changeCurrentTree, currentTree } = useBuilderContext();
+
    const treePicker = useMemo(() => {
       return Object.values(serializedTreeRef.current.trees).map((tree) => ({
          name: tree.name,
@@ -13,11 +17,12 @@ export const TreePicker = () => {
    }, []);
 
    return (
-      <div className="flex">
+      <div className="flex border-0 border-b-2 border-b-white ">
          {treePicker.map(({ id, name }) => (
             <button 
                key={id}
                onClick={() => changeCurrentTree(id)}
+               className={clsx("text-white text-xl p-2", currentTree.id ===  id && isActiveClassName)}
             >
                   {name}
                </button>
