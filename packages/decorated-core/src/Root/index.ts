24d8af8852e8c2c7828e7cfa729a173b1c7045b9
darknessInMe <vladyslav.didukh.22@gmail.com>
2025-02-25 17:42:07 +0200

    redesign: create root factory

--- packages/decorated-core/src/Root/index.ts
+++ packages/decorated-core/src/Root/index.ts
@@ -1,18 +1,17 @@
-import { Root as CoreRoot, MainStructure, Modifier } from '@buildsday2/core';
+import { Root, MainStructure, Modifier } from '@buildsday2/core';
 import { MastermindTree, EnforcerTree, TechnicianTree, GhostTree, FugitiveTree } from '../Trees';
 import { INITIAL_POINTS_COUNT } from '../shared/constants';
 
-const modifier = new Modifier();
-const structure = new MainStructure(INITIAL_POINTS_COUNT, modifier);
+export class RootFactory {
+	public getRoot() {
+		const modifier = new Modifier();
+		const structure = new MainStructure(INITIAL_POINTS_COUNT, modifier);
 
-export class Root extends CoreRoot {
-	constructor() {
-		super(structure, modifier);
-
-		this.addTree(new MastermindTree(this.modifier));
-		this.addTree(new EnforcerTree(this.modifier));
-		this.addTree(new TechnicianTree(this.modifier));
-		this.addTree(new GhostTree(this.modifier));
-		this.addTree(new FugitiveTree(this.modifier));
+		return new Root(structure, modifier)
+			.addTree(new MastermindTree(modifier))
+			.addTree(new EnforcerTree(modifier))
+			.addTree(new TechnicianTree(modifier))
+			.addTree(new GhostTree(modifier))
+			.addTree(new FugitiveTree(modifier));
 	}
 }
