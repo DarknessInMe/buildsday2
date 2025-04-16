66551697a92aaf6686b728acadede9ba0244c6c1
Vladyslav Didukh <vladyslav.didukh.22@gmail.com>
2025-04-16 17:39:27 +0300

    ui for meta tab

--- packages/next-app/src/components/TreeSidebar/MetaTab.tsx
+++ packages/next-app/src/components/TreeSidebar/MetaTab.tsx
@@ -1,7 +1,36 @@
 import React, { memo } from 'react';
 
 export const MetaTab = memo(() => {
-	return <div>MetaTab</div>;
+	return (
+		<form>
+			<div className="mb-3">
+				<label
+					htmlFor="title"
+					className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+					Build Name
+				</label>
+				<input
+					id="title"
+					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					placeholder="Enter build name"
+					required
+				/>
+			</div>
+			<div className="mb-3">
+				<label
+					htmlFor="description"
+					className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+					Build Description
+				</label>
+				<textarea
+					id="description"
+					rows={5}
+					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					placeholder="Enter build description"
+				/>
+			</div>
+		</form>
+	);
 });
 
 MetaTab.displayName = 'MetaTab';
